b7465406be5a80303f5f79a19f38ddd0a39777eb
Maison Kasprick <maison.kasprick@gmail.com>
2024-10-30 15:14:04 -0500

    Created the new files
    Created the GUI and main py files for the program

---
+++ GUI.py
@@ -0,0 +1,3 @@
+# This is the GUI of the application
+# Maison Kasprick - 10/30/2024
+# Version 1.0
---
+++ main.py
@@ -0,0 +1,3 @@
+# This is where all the main code runs
+# Maison Kasprick - 10/30/2024
+# Version 1.0
