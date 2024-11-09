4b8486077605fc605adcff71b42a624ee6f61081
Maison Kasprick <maison.kasprick@gmail.com>
2024-11-08 23:10:32 -0600

    Created the video info function
    Created the video info function, so we could grab the title of the downloaded video

--- main.py
+++ main.py
@@ -6,9 +6,47 @@
 import tkinter
 import customtkinter
 import YouTube
+from yt_dlp import YoutubeDL
 from yt_dlp import DownloadError
 from requests import HTTPError
 
+
+# Grab info of YouTube video
+def video_info(youtube_link: str) -> None:
+    """This is a function to get the information from the YouTube video 
+
+    Args:
+        youtube_link (str): Provided youtube link
+    """
+    # Test to see if YouTube link provided is valid
+    if YouTube.link_validation(youtube_link) is False:
+        finish_label.configure(text = f'"{youtube_link}" is not a valid YouTube link')
+        return
+    
+    # Attempts to grab information about the YouTube video
+    try:
+        # Settings for download
+        download_options = {}
+            
+        # Grabs title of video
+        with YoutubeDL(download_options) as youtube_download:
+            info = youtube_download.extract_info(youtube_link, download = False)
+        
+        # Informs user of video that is downloaded
+        video_label.configure(text = f"Title: {info['title']}")
+        
+    # Informs the user of any errors has occured when downloading
+    except HTTPError as he:
+        if he.code == 404:
+            finish_label.configure(text = f'HTTP Error from {youtube_link}')
+        
+        finish_label.configure(text = 'Unkown HTTP error')
+    
+    except DownloadError:
+        finish_label.configure(text = 'Error downloading {youtube_link}')
+    
+    except Exception:
+        finish_label.configure(text = 'Unkown error')
 
 # Downloads YouTube video
 def mp3() -> None:
@@ -20,26 +58,20 @@ def mp3() -> None:
     if youtube_link == '':
         return finish_label.configure(text = 'Please input a youtube link')
     
-    # Clears the label for the user
+    # Clears the labels for the user
     finish_label.configure(text = '')
-    
+    video_label.configure(text = '')
+
+    # Informs the user that it is attempting the download of the link
+    finish_label.configure(text = 'Attempting to download link')
+        
     # Runs the download_mp3 function from YouTube
-    try:
-        # Informs the user that it is attempting the download of the link
-        finish_label.configure(text = 'Attempting to download link')
-        return_str = YouTube.download_mp3(youtube_link)
-        finish_label.configure(text = return_str)
-        
-    # Informs the user of any errors has occured when downloading
-    except HTTPError:
-        finish_label.configure(text = return_str)
-        
-    except DownloadError:
-        finish_label.configure(text = return_str)
+    return_str = YouTube.download_mp3(youtube_link)
+    finish_label.configure(text = return_str)
+
+    # Gets information of video
+    video_info(youtube_link)
     
-    except Exception:
-        finish_label.configure(text = return_str)
-        
         
 # Downloads YouTube video
 def mp4() -> None:
@@ -51,25 +83,19 @@ def mp4() -> None:
     if youtube_link == '':
         return finish_label.configure(text = 'Please input a youtube link')
     
-    # Clears the label for the user
+    # Clears the labels for the user
     finish_label.configure(text = '')
+    video_label.configure(text = '')
     
-    # Runs the download_mp3 function from YouTube
-    try:
-        # Informs the user that it is attempting the download of the link
-        finish_label.configure(text = 'Attempting to download link')
-        return_str = YouTube.download_mp4(youtube_link)
-        finish_label.configure(text = return_str)
-        
-    # Informs the user of any errors has occured when downloading
-    except HTTPError:
-        finish_label.configure(text = return_str)
-        
-    except DownloadError:
-        finish_label.configure(text = return_str)
+    # Informs the user that it is attempting the download of the link
+    finish_label.configure(text = 'Attempting to download link')
     
-    except Exception:
-        finish_label.configure(text = return_str)
+    # Runs the download_mp4 function from YouTube
+    return_str = YouTube.download_mp4(youtube_link)
+    finish_label.configure(text = return_str)
+    
+    # Gets information of video
+    video_info(youtube_link)
 
 
 # Clears text from GUI
@@ -81,6 +107,7 @@ def clear() -> None:
     
     # Clears label text for user
     finish_label.configure(text = '')
+    video_label.configure(text = '')
 
 
 # GUI creation code
@@ -95,7 +122,7 @@ if __name__ == '__main__':
     app.title('YouTube Downloader')
 
     # UI Elements
-    title = customtkinter.CTkLabel(app, text = 'Insert a YouTube link')
+    title = customtkinter.CTkLabel(app, text = 'Insert a YouTube video link')
     title.pack(padx = 10, pady = 10)
 
     # Link input
@@ -106,6 +133,10 @@ if __name__ == '__main__':
     # Finished Downloading
     finish_label = customtkinter.CTkLabel(app, text = '')
     finish_label.pack()
+    
+    # YouTube video downloaded
+    video_label = customtkinter.CTkLabel(app, text = '')
+    video_label.pack()
 
     # Download buttons
     button_mp3 = customtkinter.CTkButton(app, text = 'Download MP3', command = mp3)
